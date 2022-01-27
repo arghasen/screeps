965c7b919e42817055331b45baab205815cf50f4
Argha Sen <iamslowdeath@gmail.com>
2022-01-27 19:21:09 +0530

    add energy pickup from containers or drops whichever is nearer

--- utils.js
+++ utils.js
@@ -1,35 +1,63 @@
-var utils = {
 
-    /** @param {Creep}  creep **/
-    harvestEnergy: function (creep) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if (creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
-            var res = creep.moveTo(sources[creep.memory.source], { visualizePathStyle: { stroke: '#ffaa00' } });
-            if (res == ERR_NO_PATH) {
-                creep.move(_.random(1, 8)); // FIXME: move in a random direction if no path found
-            }
-        }
-    },
-    /** @param {Creep}  creep **/
-    pickupEnergy: function (creep) {
-      // var droppedResources = creep.room.find(FIND_DROPPED_RESOURCES);
-      // var sources = creep.room.find(FIND_SOURCES)
-      //  var allEnergy = droppedResources.concat(sources)
-        var source = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES,
-            {filter:(sources)=>sources.resourceType==RESOURCE_ENERGY && sources.amount >= 150});
-        if (source) {
-            if (creep.pickup(source) == ERR_NOT_IN_RANGE) {
-                var res = creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
-            }
-        } 
-        else
-        {
-            // if(creep.memory.role =='harvester')
-            // {
-            //     utils.harvestEnergy(creep);
-            // }
-        }
+/** @param {Creep}  creep **/
+function getClosestEnergySource(creep) {
+    var droppedResources = creep.room.find(FIND_DROPPED_RESOURCES);
+    var structures = creep.room.find(FIND_STRUCTURES);
+    console.log(structures);
+    var energyResources = _.filter(
+        droppedResources,
+        (droppedResource) => droppedResource.resourceType == RESOURCE_ENERGY &&
+            droppedResource.amount >= creep.store.getFreeCapacity()
+    );
+    var containers = _.filter(
+        structures,
+        (structure) => structure.structureType == STRUCTURE_CONTAINER &&
+            structure.store.getUsedCapacity() >= creep.store.getFreeCapacity()
+    );
+    var allEnergy = droppedResources.concat(containers);
+    console.log(allEnergy);
+    var closestSource = creep.pos.findClosestByPath(allEnergy);
+    console.log(creep.name + closestSource);
+    return closestSource;
+}
+
+
+var utils = {
+  /** @param {Creep}  creep **/
+  harvestEnergy: function (creep) {
+    var sources = creep.room.find(FIND_SOURCES);
+    if (creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
+      var res = creep.moveTo(sources[creep.memory.source], {
+        visualizePathStyle: { stroke: "#ffaa00" },
+      });
+      if (res == ERR_NO_PATH) {
+        creep.move(_.random(1, 8)); // FIXME: move in a random direction if no path found
+      }
     }
+  },
+  /** @param {Creep}  creep **/
+  pickupEnergy: function (creep) {
+    var closestSource = getClosestEnergySource(creep);
+
+    if (closestSource) {
+      if (closestSource instanceof Resource) {
+        if (creep.pickup(closestSource) == ERR_NOT_IN_RANGE) {
+          var res = creep.moveTo(closestSource, {
+            visualizePathStyle: { stroke: "#ffaa00" },
+          });
+        }
+      } else if (closestSource instanceof Structure) {
+        if (
+          creep.withdraw(closestSource, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE
+        ) {
+          var res = creep.moveTo(closestSource, {
+            visualizePathStyle: { stroke: "#ffaa00" },
+          });
+        }
+      }
+    }
+  },
 };
 
 module.exports = utils;
+
